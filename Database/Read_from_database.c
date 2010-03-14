1db0283c456784d406feb4160ec6c59e319f987d
mihaytza.ort <mihaytza.ort@fa414398-2c71-11df-af82-b5ce7a3b21f7>
2010-03-14 23:49:33 +0000

    Am adaugat Folderul Database in care o sa punem toate fisierele din care o sa trebuiasca sa citim date. Am adaugat 2 fisiere : All_moves.in cu toate pozitiile posibile pentru toate piesele din toate pozitiile posibile:)) (in afara de pioni) si inca unu cu pozitiile initiale ale pieselor. Am scris si 2 functii de citire din fisiere, in ordinea care trebuie.... Te rog io boss sa te uiti oleaca pacolo daca e bine ce am scris ... ca n am testat..

---
+++ Database/Read_from_database.c
@@ -0,0 +1,66 @@
+/* General description :
+ *
+ */
+
+/* ----- System #includes ----- */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ----- Local #inlcudes ----- */
+
+/* ---- Macro #define ---- */
+
+/* --- Types --- */
+
+/* --- Procedures --- */
+
+void Read_all_possible_moves(FILE* fin, BITMAP** King_moves,
+		BITMAP** Queen_moves, BITMAP** Rock_moves, BITMAP**Bishop_moves,
+		BITMAP** Knight_moves) {
+
+	if (fin = fopen("All_moves.in", "rb") == NULL) {
+		printf("Eroare la deschiderea fisierului All_moves.in");
+		exit(0);
+	}
+
+	int i, j;
+
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+
+			fread(&King_moves[i][j], 8, 1, fin);
+			fread(&Queen_moves[i][j], 8, 1, fin);
+			fread(&Rock_moves[i][j], 8, 1, fin);
+			fread(&Bishop_moves[i][j], 8, 1, fin);
+			fread(&Knight_moves[i][j], 8, 1, fin);
+		}
+	}
+	fclose(fin);
+}
+
+void Read_initial_state(FILE* fin, BITMAP W_Pos, BITMAP W_Ki, BITMAP W_Q,
+		BITMAP W_R, BITMAP W_B, BITMAP W_Kn, BITMAP B_Pos, BITMAP B_Ki,
+		BITMAP B_Q, BITMAP B_R, BITMAP B_B, BITMAP B_Kn) {
+
+	if (fin = fopen("Initial_positions.in", "rb") == NULL) {
+		printf("Eroare la deschiderea fisierului Initial_positions.in");
+		exit(0);
+	}
+
+	fread(&W_Pos, 8, 1, fin);
+	fread(&W_Ki, 8, 1, fin);
+	fread(&W_Q, 8, 1, fin);
+	fread(&W_R, 8, 1, fin);
+	fread(&W_B, 8, 1, fin);
+	fread(&W_Kn, 8, 1, fin);
+
+	fread(&B_Pos, 8, 1, fin);
+	fread(&B_Ki, 8, 1, fin);
+	fread(&B_Q, 8, 1, fin);
+	fread(&B_R, 8, 1, fin);
+	fread(&B_B, 8, 1, fin);
+	fread(&B_Kn, 8, 1, fin);
+
+	fclose(fin);
+}
+
