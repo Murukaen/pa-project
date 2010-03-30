5261788b015a6323982bc3e22e28de0c7569e651
razzvan.savu <razzvan.savu@fa414398-2c71-11df-af82-b5ce7a3b21f7>
2010-03-30 18:12:20 +0000

    1) Test for update_state

--- trunk/Test/test_update_state/test_update_state.c
+++ trunk/Test/test_update_state/test_update_state.c
@@ -3,23 +3,45 @@
 #include "../../cur_state/cur_state.h"
 #include "../../state/state.h"
 #include "../../Flags/flags.h"
+#include "../../move/move.h"
+#include "../../state/state.h"
 
-#define FILE_OUT_INITIAL_STATE "Test/test_update_state/initial_state.out"
 
-int main ( void ) {
+int main ( int argc , char ** argv  ) {
+	
+		if(argc != 3 ) {
+				
+				printf("Usage %s file.in file.out\n" , argv[0] );
+				return 0;
+		}
 	
 		/* Inits */
 		BM_init();
 		flags_init();
-		update_state_init ();
 		/* END Inits */
 		
-		/* Test */
-		FILE *fout = fopen (FILE_OUT_INITIAL_STATE , "w");
+		/* Vars */
+		STATE st;
+		MOVE mov;
+		/* END Vars */
+		
+		/* Read */
+		FILE *fin = fopen ( argv[1] , "r" );
+		mov = move_read ( fin );
+		st = state_read ( fin );
+		fclose(fin);
+		/* END Read */
+		
+		cur_state_set ( st );
+		update_state ( mov );
+		
+		/* Output */
+		FILE *fout = fopen (argv[2] , "w");
+		//move_print ( mov , fout );  VERIFY ONLY
+		//state_print ( st , fout );  VERIFY ONLY
 		state_print ( cur_state_get () , fout );
 		fclose(fout);
-		/* END Test */
+		/* END Output */
 		
 		return 0;
 }
-
